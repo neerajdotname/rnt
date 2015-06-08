d2d4fcc9759bba17d6aee39386fbefca20b3a6ae
Neeraj Singh <neeraj@bigbinary.com>
2015-06-07 22:44:33 -0400

    wip

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,7 @@ var githubNotetaker = React.createClass({
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to github
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
