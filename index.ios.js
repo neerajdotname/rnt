e7282875e84b661087fc424403b3570eefd026f5
Neeraj Singh <neeraj@bigbinary.com>
2015-06-07 23:01:51 -0400

    WIP

--- index.ios.js
+++ index.ios.js
@@ -5,6 +5,7 @@
 'use strict';
 
 var React = require('react-native');
+var Main = require('./App/Components/Main');
 
 var {
   AppRegistry,
@@ -17,18 +18,13 @@ var {
 var githubNotetaker = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to github
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Github notetaker',
+          component: Main
+        }} />
+
     );
   }
 });
@@ -36,20 +32,8 @@ var githubNotetaker = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    backgroundColor: '#111111',
+  }
 });
 
 AppRegistry.registerComponent('githubNotetaker', () => githubNotetaker);
